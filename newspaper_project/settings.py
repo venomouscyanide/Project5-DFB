23c7f6954704e478e8ec475c32c87960b429b1d1
Paul Louis <iampaullouis@gmail.com>
2018-12-12 17:37:43 +0530

    starting articles app work

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'crispy_forms',#3rd party
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
+    'articles.apps.ArticlesConfig',
 
 ]
 
@@ -111,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Calcutta'
 
 USE_I18N = True
 
